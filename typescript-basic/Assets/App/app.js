a735e8e83fd391cee7678c25e903b76b8acf61d0
itay <itay@gmail.com>
2024-10-23 15:24:55 +0300

    learn js

--- typescript-basic/Assets/App/app.js
+++ typescript-basic/Assets/App/app.js
@@ -1,2 +1,17 @@
+//Output to the page
 alert("My name is Itay Leon");
 document.write("My name is itay leon");
+document.write("<br>");
+document.write(`The sum of ${12} and ${23} is ${12+23}.`);
+document.write("<br>");
+document.write(`The sum of ${20} and ${100} is ${20+100}.`);
+
+//input from the user
+var name = prompt("What is your name?");
+document.write("<br>");
+document.write(`Hello ${name}`); 
+
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+document.write("<br>");
+document.write(`Hello ${fName} ${lName}`);
