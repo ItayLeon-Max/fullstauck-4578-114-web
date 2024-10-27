16c14542c0acb803515bf57edb5ff668d57e2aa3
itay <itay@gmail.com>
2024-10-27 11:51:29 +0200

    change

--- typescript-basic/Assets/App/app.js
+++ typescript-basic/Assets/App/app.js
@@ -25,33 +25,33 @@
 //     alert(`Hello ${fName} ${lName}` + "\n" + `You are ${age} years old`);
 // }
 
-//ex1
-var grade = prompt("What is your grade?");
-if (grade >= 60) {
-    document.write("You passed");
-} else {
-    document.write("You failed");
-}
-document.write("<br>");
+// //ex1
+// var grade = prompt("What is your grade?");
+// if (grade >= 60) {
+//     document.write("You passed");
+// } else {
+//     document.write("You failed");
+// }
+// document.write("<br>");
 
-//ex2
-var num = prompt("Enter a number");
-if(num > 0) {
-    document.write("Positive number");
-} else if(num < 0) {
-    alert("Negative number");
-} else {
-    document.write("Zero");
-}
-document.write("<br>");
+// //ex2
+// var num = prompt("Enter a number");
+// if(num > 0) {
+//     document.write("Positive number");
+// } else if(num < 0) {
+//     alert("Negative number");
+// } else {
+//     document.write("Zero");
+// }
+// document.write("<br>");
 
-//ex3
-var grade1 = prompt("Enter grade 1");
-if(grade1 >=0 && grade <=100) {
-    document.write("Valid grade");
-} else {
-    document.write("Invalid grade");
-}
+// //ex3
+// var grade1 = prompt("Enter grade 1");
+// if(grade1 >=0 && grade <=100) {
+//     document.write("Valid grade");
+// } else {
+//     document.write("Invalid grade");
+// }
 
 //ex4
 var price = prompt("Enter the price");
