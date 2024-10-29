37524986e3e27b240fa2d698155c490214c9f2dc
itay <itay@gmail.com>
2024-10-29 21:27:44 +0200

    fix

--- 7-BOOM/Assets/JS/app.js
+++ 7-BOOM/Assets/JS/app.js
@@ -1,14 +1,24 @@
 //7-BOOM Game
-//input: number- 39
-//output: 1 2 3 4 5 6 BOOM 8 9 10 11 12 13 14 BOOM 16 17 18 19 20 21 22 BOOM 24 25 26 27 28 29 BOOM BOOM BOOM BOOM BOOM 30 31 32 33 34 35 36 37 38 39
+//Ask the user to enter a number.
+//If the number is divisible by 7, alert "BOOM!!!"
+//Otherwise, alert "You lose"
+//To make it more interesting, you can add the following rules:
+//If the number contains 7, alert "BOOM!!!"
+//Example: 7, 17, 27, 37, 47, 57, 67, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 87, 97
+//Note: The rules must be followed in the same order.
+//Example: If the number is 77, it is divisible by 7 and contains 7. Both rules apply.
+
 let num = parseInt(prompt("Enter a number: "));
 let i = 1;
 while(i <= num){
-    if(i % 7 == 0 || i.toString().includes("7")){
+    //Integer: 47 ToString ---> String: "47" (.includes 7) test if have 7 on it or not on the string string
+    if(i % 7 == 0 || i.toString().includes("7")){ 
+        //if the number is divisible by 7 or contains 7
         document.write(" BOOM ");
     }else{
+        //if the number is not divisible by 7 or contains 7
         document.write(i + " ");
     }
     i++;
 }
-//8-Print the following pattern
+
