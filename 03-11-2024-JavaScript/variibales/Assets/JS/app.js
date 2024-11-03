a266c96bba49d8e838656b9cf10d6523b51d05b3
itay <itay@gmail.com>
2024-11-03 10:43:20 +0200

    added to github

---
+++ 03-11-2024-JavaScript/variibales/Assets/JS/app.js
@@ -0,0 +1,7 @@
+// historic way of using variables
+var companyName = 'KSP';
+document.write(companyName + '<br>' + companyName);
+alert(companyName + "\n" + ' is a great company');
+console.log(companyName + "\n" + ' is a great company');
+
+document.write(companyName + " is a great company" + '<br>');
