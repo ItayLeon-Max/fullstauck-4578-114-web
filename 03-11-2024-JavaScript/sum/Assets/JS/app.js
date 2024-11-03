e9e59a1e0c57a325ab5f67ca66eb82d766d6de44
itay <itay@gmail.com>
2024-11-03 15:18:05 +0200

    done

--- 03-11-2024-JavaScript/sum/Assets/JS/app.js
+++ 03-11-2024-JavaScript/sum/Assets/JS/app.js
@@ -1,7 +1,7 @@
-let num;
+let num = +prompt('Enter a number');
 let sum = 0;
 while(num !== 0){
-    num = +prompt('Enter a number');
     sum += num;
+    num = +prompt('Enter a number');
 }
 document.write(`Sum of numbers is ${sum}`);
