604d9a6f2e44d6ba9e66b07615980481d54fe443
itay <itay@gmail.com>
2024-11-03 11:39:52 +0200

    s

---
+++ 03-11-2024-JavaScript/if-else/Assets/JS/app.js
@@ -0,0 +1,15 @@
+// const age = prompt(`Enter your age: `);
+// // document.write(`You are ${age} years old.`);
+
+// if(age >= 13){
+//     alert(`Welcome`);
+//     alert`today have sale of articles for children`;
+// } else {
+//     alert(`go home child`);
+//     alert(`and the next time bring your parents`);
+// }
+
+// age >= 13 ? alert(`Welcome`) : alert(`go home child`);
+
+
+
