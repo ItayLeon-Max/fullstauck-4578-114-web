bb6b0d3d193a0e8ccd472de3fdecae4bae46a2c7
itay <itay@gmail.com>
2024-11-03 11:00:21 +0200

    .

--- 03-11-2024-JavaScript/variibales/Assets/JS/app.js
+++ 03-11-2024-JavaScript/variibales/Assets/JS/app.js
@@ -6,3 +6,8 @@ console.log(companyName + "\n" + ' is a great company');
 
 document.write(companyName + " is a great company" + '<br>');
 document.write(`${companyName} is a great company` + '<br>');
+
+// let inputText = document.createElement('input');
+// inputText.setAttribute('type', 'text');
+// document.body.appendChild(inputText);   
+// document.write('<br>');
