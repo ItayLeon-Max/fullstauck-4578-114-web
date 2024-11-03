4b28188578bd80884466cdb56eb95789e66c54ad
itay <itay@gmail.com>
2024-11-03 12:20:50 +0200

    a

---
+++ 03-11-2024-JavaScript/if-else exp1/Assets/JS/app.js
@@ -0,0 +1,15 @@
+const firstNumber = +(prompt('Enter the first number: '));
+const secondNumber = +(prompt('Enter the second number: '));
+
+//use short if else
+// firstNumber > secondNumber ? alert(`The first number ${firstNumber} is bigger`) : alert(`The second number ${secondNumber} is bigger`);
+
+//use if else classic
+if (firstNumber > secondNumber) {
+    alert(`The first number ${firstNumber} is bigger`);
+} else {
+    alert(`The second number ${secondNumber} is bigger`);
+}
+
+
+
