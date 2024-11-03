64fc2f995f33e6e37d0de06df56af8be52c0d3ce
itay <itay@gmail.com>
2024-11-03 13:52:20 +0200

    done

---
+++ 03-11-2024-JavaScript/if-else exp4/Assets/JS/app.js
@@ -0,0 +1,24 @@
+const num = +prompt('Enter a number: ');
+switch(num) {
+    case 1:
+        document.write('one');
+        break;
+    case 2:
+        document.write('two');
+        break;
+    case 3:
+        document.write('three');
+        break;
+    case 4:
+        document.write('four');
+        break;
+    case 5:
+        document.write('five');
+        break;
+    default:
+        document.write('Invalid number');
+        break;
+}
+    
+
+
