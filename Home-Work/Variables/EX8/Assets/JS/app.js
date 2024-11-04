b453559413955f01cabd7adb5f4dc1304d751f4d
itay <itay@gmail.com>
2024-11-04 11:03:54 +0200

    done EX Of Variables

---
+++ Home-Work/Variables/EX8/Assets/JS/app.js
@@ -0,0 +1,4 @@
+const L = +prompt('Enter the length of the box: ');
+const W = +prompt('Enter the width of the box: ');
+const H = +prompt('Enter the height of the box: ');
+document.write(`V = ${L * W * H}`);
