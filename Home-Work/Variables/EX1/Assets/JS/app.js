dd57600a6a80ef5e678963822f02fea4c7158198
itay <itay@gmail.com>
2024-11-04 10:53:11 +0200

    added 2 EX

---
+++ Home-Work/Variables/EX1/Assets/JS/app.js
@@ -0,0 +1,2 @@
+const num = +prompt('Enter a number: ');
+document.write(`${num} ** 2 = ${num ** 2}`);
---
+++ Home-Work/Variables/EX2/Assets/JS/app.js
@@ -0,0 +1,4 @@
+const firstNumber = +prompt('Enter the first number');
+const secondNumber = +prompt('Enter the second number');
+const thirdNumber = +prompt('Enter the third number');
+document.write(`${firstNumber} * ${secondNumber} * ${thirdNumber} = ${firstNumber * secondNumber * thirdNumber}`);
