5bc2af69b4bf4f3bfbf7ee2d14450cc9caa2f731
itay <itay@gmail.com>
2024-11-05 10:35:16 +0200

    added more

--- 03-11-2024-JavaScript/Home-Work/Conditions/Calculator/Assets/JS/app.js
+++ 03-11-2024-JavaScript/Home-Work/Conditions/Calculator/Assets/JS/app.js
@@ -2,12 +2,12 @@
 //Date: 2021-09-30
 //Description: This is a simple calculator app that can perform basic arithmetic operations and some scientific operations.
 //The user is prompted to enter the operator and the numbers to perform the operation.
-//The user can perform the following operations: addition, subtraction, multiplication, division, modulo, cosine, sine, tangent, logarithm, square root, square, cube, and power.
+//The user can perform the following operations: addition, subtraction, multiplication, division, modulo, cosine, sine, tangent, logarithm, square root, square, cube, and power, factorial, and a system of linear equations.
 //The user can enter the operator and the numbers to perform the operation.
 //*******************************************************************************
 
 alert("Hello, welcome to this calculator app!");
-alert("Enter the operator: +, -, *, /, %, cos, sin, tan, log, sqrt^2, 2power, 3power, power");
+alert("Enter the operator: +, -, *, /, %, cos, sin, tan, log, sqrt^2, 2power, 3power, power, !, A system of linear equations(LE), quadratic equation(QE), Third degree equation(TE)");
 const operator = prompt("Enter the operator: ");
 let num1;
 let num2;
@@ -127,7 +127,85 @@ switch (operator) {
         num2 = +prompt("Enter the power: ");
         alert(`${num1}^${num2} = ${Math.pow(num1, num2)}`);
         alert(`Thanks for using this calculator app!`);
-        break;                       
+        break; 
+    case "!":
+        num1 = +prompt("Enter the number: ");
+        let factorial = 1;
+        if(num1 < 0) {
+            alert("Invalid input for factorial");
+            break;
+        } else {
+            for(let i = 1; i <= num1; i++){
+                factorial *= i;
+            }
+            alert(`${num1}! = ${factorial}`);
+            alert(`Thanks for using this calculator app!`);
+            break;
+        }   
+    case "LE":
+    case "le":    
+        let a1 = +prompt("Enter the first coefficient of x: ");
+        let b1 = +prompt("Enter the second coefficient of x: ");
+        let c1 = +prompt("Enter the constant of the first equation: ");
+        let a2 = +prompt("Enter the first coefficient of y: ");
+        let b2 = +prompt("Enter the second coefficient of y: ");
+        let c2 = +prompt("Enter the constant of the second equation: ");
+        let det = a1 * b2 - a2 * b1;
+        let detX = c1 * b2 - c2 * b1;
+        let detY = a1 * c2 - a2 * c1;
+        if(det === 0) {
+            alert("The system of linear equations has no solution.");
+        } else {
+            let x = detX / det;
+            let y = detY / det;
+            alert(`The solution of the system of linear equations is x = ${x} and y = ${y}`);
+        }
+        alert(`Thanks for using this calculator app!`);
+        break; 
+    case "QE":
+    case "qe":    
+        let a = +prompt("Enter the coefficient of x^2: ");
+        let b = +prompt("Enter the coefficient of x: ");
+        let c = +prompt("Enter the constant: ");
+        let discriminant = b * b - 4 * a * c;
+        if(discriminant > 0) {
+            let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            alert(`The solutions of the quadratic equation are x1 = ${x1} and x2 = ${x2}`);
+        } else if(discriminant === 0) {
+            let x = -b / (2 * a);
+            alert(`The solution of the quadratic equation is x = ${x}`);
+        } else {
+            let realPart = -b / (2 * a);
+            let imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+            let x1 = `${realPart} + ${imaginaryPart}i`;
+            let x2 = `${realPart} - ${imaginaryPart}i`;
+            alert(`The solutions of the quadratic equation are x1 = ${x1} and x2 = ${x2}`);
+        }
+        alert(`Thanks for using this calculator app!`);
+        break;   
+    case "TE":
+    case "te":    
+         a = +prompt("Enter the coefficient of x^3: ");
+         b = +prompt("Enter the coefficient of x^2: ");
+         c = +prompt("Enter the coefficient of x: ");
+        let d = +prompt("Enter the constant: ");
+        let root1;
+        let root2;
+        let root3;
+        discriminant = b * b - 3 * a * c;
+        let temp1 = (3 * a * c - b * b) / (3 * a * a);
+        let temp2 = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
+        let temp3 = temp2 * temp2 / 4 + temp1 * temp1 * temp1 / 27;
+        let temp4 = Math.sqrt(-temp3);
+        let temp5 = Math.acos(-temp2 / (2 * temp4));
+        let temp6 = Math.pow(temp4, 1/3);
+        root1 = 2 * temp6 * Math.cos(temp5 / 3) - b / (3 * a);
+        root2 = 2 * temp6 * Math.cos((temp5 + 2 * Math.PI) / 3) - b / (3 * a);
+        root3 = 2 * temp6 * Math.cos((temp5 + 4 * Math.PI) / 3) - b / (3 * a);
+        alert(`The solutions of the third degree equation are x1 = ${root1}, x2 = ${root2}, and x3 = ${root3}`);
+        alert(`Thanks for using this calculator app!`);
+        break;                   
     default:
         alert("Invalid operator");
         break;                  
