1b11c0666180af49c4b56cba85a852d0b1822473
Itay Leon <itayleon81@walla.com>
2024-12-08 09:27:56 +0200

    Refactor average calculation to improve error handling and update UI interaction

--- 2024-12-03 exception/HomeWork/EX1/app.js
+++ 2024-12-03 exception/HomeWork/EX1/app.js
@@ -1,23 +1,20 @@
 "use strict";
 (()=>{
     const getAvg = (arr) => {
-        if(arr === null || arr === undefined) throw new Error('Array is required');
+        if(typeof arr === 'undefined') throw new Error('Array is required');
+        if(arr === null) throw new Error('Array is required');
         if(!Array.isArray(arr)) throw new Error('Array is required');
         if(arr.length === 0) throw new Error('Array is empty');
 
         return arr.reduce((acc, val) => acc + val, 0) / arr.length;
     }
-    const arrSize = +prompt('Enter the size of the array');
-    const arr = [];
-    for(let i = 0; i < arrSize; i++) {
-        arr.push(+prompt(`Enter element ${i + 1}`));
-    }
 
-    try {
-        const avg = getAvg(arr);
-        console.log(`The average of the array is ${avg}`);
-        console.log(arr);
-    } catch (error) {
-        console.error(error.message);
-    }
+    const btn = document.getElementById('invoker');
+    btn.addEventListener('click' , ()=>{
+        try{
+            console.log(getAvg([1,2,3,4,5]));
+        } catch(err){
+            console.log(err.message);
+        }
+    })
 })();
