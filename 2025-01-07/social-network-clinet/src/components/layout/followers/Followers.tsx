2b262f46ffa506c3cc381786533324f859649609
Itay Leon <itayleon81@walla.com>
2025-01-07 13:23:43 +0200

    Add initial layout components and styles for social network client

---
+++ 2025-01-07/social-network-clinet/src/components/layout/followers/Followers.tsx
@@ -0,0 +1,9 @@
+import './Followers.css';
+
+export default function Followers(): JSX.Element {
+    return (
+        <div className='Followers'>
+        <h1>Followers</h1>
+        </div>
+    );
+}
---
+++ 2025-01-07/social-network-clinet/src/components/layout/following/Following.tsx
@@ -0,0 +1,9 @@
+import './Following.css';
+
+export default function Following(): JSX.Element {
+    return (
+        <div className='Following'>
+        <h1>Following</h1>
+        </div>
+    );
+}
