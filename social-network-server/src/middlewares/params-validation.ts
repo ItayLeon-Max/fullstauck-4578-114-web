38ed3588e27daeb295ed6c20787f255b79ebca21
Itay Leon <itayleon81@walla.com>
2025-02-23 08:36:51 +0200

    Refactor error handling in paramsValidation and requireAuth middlewares to use TwitterError with appropriate status codes

--- social-network-server/src/middlewares/params-validation.ts
+++ social-network-server/src/middlewares/params-validation.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ObjectSchema } from "joi";
+import TwitterError from "../errors/twitter-erros";
+import statusCode from "http-status-codes";
+
 
 export default function paramsValidation(validator: ObjectSchema) {
     return async function (req: Request, res: Response, next: NextFunction) {
@@ -7,10 +10,7 @@ export default function paramsValidation(validator: ObjectSchema) {
             req.params = await validator.validateAsync(req.params)
             next()
         } catch (e) {
-            next({
-                status: 422, // 422 http code for Unprocessable Entity
-                message: e.message
-            })
+            next(new TwitterError(statusCode.BAD_REQUEST, e.message))
         }
     }
 }
--- social-network-server/src/middlewares/require-auth.ts
+++ social-network-server/src/middlewares/require-auth.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Response } from "express";
 import TwitterError from "../errors/twitter-erros";
 import { AuthenticatedRequest } from "./auth";
+import statusCode from "http-status-codes";
+
 
 export default function requireAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
     if (!req.userId) {
-        return next(new TwitterError(401, "Unauthorized - please login first"));
+        return next(new TwitterError(statusCode.UNAUTHORIZED, 'Unauthorized'));
     }
     next();
 }
