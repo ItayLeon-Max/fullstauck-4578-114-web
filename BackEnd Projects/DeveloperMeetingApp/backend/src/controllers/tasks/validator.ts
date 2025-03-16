81393e452737f56509d4bc5d217fb72cef89bd66
Itay Leon <itayleon81@walla.com>
2025-03-16 08:49:29 +0200

    Update task validator: enforce minimum description length and change assignedTo type to string

--- BackEnd Projects/DeveloperMeetingApp/backend/src/controllers/tasks/validator.ts
+++ BackEnd Projects/DeveloperMeetingApp/backend/src/controllers/tasks/validator.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const createTaskValidator = Joi.object({
-    description: Joi.string().required(),
+    description: Joi.string().min(5).required(),
     meetingId: Joi.string().optional(),
-    assignedTo: Joi.number().required(),
+    assignedTo: Joi.string().required(),
     status: Joi.string().optional(),
     deadline: Joi.date().required(),
 });
