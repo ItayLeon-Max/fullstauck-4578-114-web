23beefa8ead939907404a183ee3d69bc273b900f
Itay Leon <itayleon81@walla.com>
2025-03-25 14:55:13 +0200

    Refactor comment creation logic to enhance user data retrieval; update populate method for comments

--- 2025-03-23-mongo-db/01-mongo-db/backend/src/controllers/comments/controller.ts
+++ 2025-03-23-mongo-db/01-mongo-db/backend/src/controllers/comments/controller.ts
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 import socket from "../../io/io";
 import SocketMessages from "socket-enums-shaharsol";
 import { PostModel } from "../../models/post";
-import { date } from "joi";
 
 export async function createComment(req: Request<{postId: string}>, res: Response, next: NextFunction) {
     try {
@@ -11,7 +10,17 @@ export async function createComment(req: Request<{postId: string}>, res: Respons
 
         const { postId } = req.params 
 
-        const comment = await PostModel.findByIdAndUpdate(postId, { $push: { comments: { userId, body: req.body.body, createdAt: new Date(), user: userId } } }, { new: true }).populate( 'user','comments.user')
+        const comment = await PostModel.findByIdAndUpdate(
+            postId, 
+            {
+                 $push: {
+                     comments: {
+                         userId,
+                          body: req.body.body,
+                           createdAt: new Date(),
+                            user: userId 
+                        } } 
+                }, { new: true }).populate([{ path: 'user' }, { path: 'comments.user' }])
         res.json(comment.toObject())
         // socket.emit(SocketMessages.NEW_COMMENT, {
         //     from: req.headers['x-client-id'], // req.header(), req.get()
